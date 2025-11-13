43f6c50b6aafa7714d3057133b321bd7ad608997
Sut Pi <pijlsut@gmail.com>
2025-11-13 11:58:39 +0700

    from sut pi branch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 console.log("index.js");
-console.log("version");
+console.log("version sut pi  ");
 
 console.log("add form Sut pi acc");
 console.log("jl sut pi branch with conflit");
+console.log("oki");
